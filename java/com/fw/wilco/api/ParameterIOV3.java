6961034805db933093d599ffaa56475c397f2bd9
dao hodac <hodac@flightwatching.com>
2014-10-28 15:58:28 +0100

    add an ID to the parameterIOV3

--- java/com/fw/wilco/api/ParameterIOV3.java
+++ java/com/fw/wilco/api/ParameterIOV3.java
@@ -4,13 +4,12 @@ package com.fw.wilco.api;
 
 public class ParameterIOV3 {
 
+	public Long id;
 	public  Double minScale;
 	public  Double maxScale;
 	public  Double minOK;
 	public  Double maxOK;
 	public  String name;
 	public  String actypeName;
-
-
 
 }
