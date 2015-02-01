ca3a3565013b1a94e1573a601e94fba19cefe3b2
dao hodac <hodac@flightwatching.com>
2015-02-01 21:20:35 +0100

    add bookmarks IO

---
+++ java/com/fw/wilco/api/BookmarkV3IO.java
@@ -0,0 +1,20 @@
+package com.fw.wilco.api;
+
+
+public class BookmarkV3IO {
+	
+	public String href;
+	
+	public String glyphicon;
+	
+	public String text;
+	
+	public String tooltip;
+	
+	public String rank;
+	
+	public boolean disabled;
+
+	public String who;
+
+}
