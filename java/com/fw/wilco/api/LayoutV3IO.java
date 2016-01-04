11df7ee62558dc5aeae2b9711ab5995f70d49fb2
dao hodac <dao.hodac@gmail.com>
2016-01-04 16:55:27 +0100

    adds a layout V3IO

--- java/com/fw/wilco/api/LayoutV3IO.java
+++ java/com/fw/wilco/api/LayoutV3IO.java
@@ -2,8 +2,15 @@ package com.fw.wilco.api;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class LayoutV3IO {
+	
+	public static class ExtSource {
+		public String varName;
+		public String urlTemplate;
+		public Integer poolPeriodSecond;
+	}
 
 	public Long id;
 	public String type;
@@ -12,5 +19,8 @@ public class LayoutV3IO {
 	public List dashboards;
 	public String reportId;
 	public Boolean canUseAlphaCallUps;
+	public ArrayList<RuleV3IO> ifts;
+	
+	public ExtSource extSource;
 
 }
