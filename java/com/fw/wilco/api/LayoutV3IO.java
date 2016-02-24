b4fb65a5a8a82348580224905768621449bc800a
dao hodac <dao.hodac@gmail.com>
2016-02-24 11:51:00 +0100

    add a layout IO export

--- java/com/fw/wilco/api/LayoutV3IO.java
+++ java/com/fw/wilco/api/LayoutV3IO.java
@@ -2,7 +2,6 @@ package com.fw.wilco.api;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class LayoutV3IO {
 	
@@ -11,15 +10,27 @@ public class LayoutV3IO {
 		public String urlTemplate;
 		public Integer poolPeriodSecond;
 	}
-
+	
+	//ieiLayout
+	public List<IeiV3IO> ieis;
+	public String engineType;
+	public String layoutFMTs;
+	public String layoutDBs;
+	
+	//uplinks
+	public List<ParameterV3IO> params;
+	
 	public Long id;
-	public String type;
+	public String actype;
+	public String dtype;
 	public ArrayList<String> dashboardIds;
 	public String name;
 	public List dashboards;
 	public String reportId;
 	public Boolean canUseAlphaCallUps;
-	public ArrayList<RuleV3IO> ifts;
+	public ArrayList<IftV3IO> ifts;
+		
+	public String uplinkTemplate;
 	
 	public ExtSource extSource;
 
